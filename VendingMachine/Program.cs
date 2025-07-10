897117ba53c17843abfdcfef331e9dbc3caa5a25
tcs-r-nakamura <rnakamura.tcs@gmail.com>
2025-07-10 13:32:14 +0900

    first commit

---
+++ VendingMachine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class VendingMachine
+{
+    private int stock; //飲み物の在庫
+
+    public VendingMachine(int stock)
+    {
+        this.stock = stock;
+    }
+
+    public void Buy()
+    {
+        if (stock == 0)
+        {
+            Console.WriteLine("売り切れ");
+        }
+        else if (stock >= 1)
+        {
+            stock--;
+            Console.WriteLine("残り" + stock + "本");
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        VendingMachine VM = new VendingMachine(2);
+        VM.Buy();
+    }
+}
