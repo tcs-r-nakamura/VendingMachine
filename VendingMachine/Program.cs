523b53c771ee26060096ec5822e500adb558171f
tcs-r-nakamura <rnakamura.tcs@gmail.com>
2025-07-10 14:31:01 +0900

    商品追加

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -2,11 +2,16 @@
 
 class VendingMachine
 {
+    private List<products> products = new List<products>();
     private int stock; //飲み物の在庫
+ 
 
     public VendingMachine(int stock)
     {
         this.stock = stock;
+        products.Add("ファンタ");
+        products.Add("グレープ");
+        products.Add("コーラ");
     }
 
     public void Buy()
@@ -18,14 +23,20 @@ class VendingMachine
         else if (stock >= 1)
         {
             stock--;
-            Console.WriteLine("残り" + stock + "本");
+            Console.WriteLine($"残り{stock}本");
         }
     }
+}
+class products
+{
+    public int stock;
+     
 }
 class Program
 {
     static void Main()
     {
+        
         VendingMachine VM = new VendingMachine(2);
         VM.Buy();
     }
