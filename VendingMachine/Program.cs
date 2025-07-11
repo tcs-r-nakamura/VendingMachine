277a82bdf5a1d0bea2a4564e305c0f93b9bf8674
tcs-r-nakamura <rnakamura.tcs@gmail.com>
2025-07-11 10:39:51 +0900

    試作

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -1,43 +1,64 @@
 ﻿using System;
-
-class VendingMachine
+using System.Collections.Generic;
+/*セッターとゲッターをつかう
+ * ポリモーフィズムを利用する
+ * カプセル化する*/
+class Product
 {
-    private List<products> products = new List<products>();
-    private int stock; //飲み物の在庫
- 
+    public string Name { get; private set; } //商品名
+    public int Stock { get; private set; } //在庫数
+    public int Price { get; private set; } //値段
 
-    public VendingMachine(int stock)
+
+    public Product(string name, int stock, int price)
     {
-        this.stock = stock;
-        products.Add("ファンタ");
-        products.Add("グレープ");
-        products.Add("コーラ");
+        Name = name;
+        Stock = stock;
+        Price = price;
     }
-
     public void Buy()
     {
-        if (stock == 0)
+        if (Stock >= 1)
         {
-            Console.WriteLine("売り切れ");
+            Stock--;
+            Console.WriteLine($"残り{Stock}本");
         }
-        else if (stock >= 1)
+        else
         {
-            stock--;
-            Console.WriteLine($"残り{stock}本");
+            Console.WriteLine("売切れ");
         }
     }
 }
-class products
-{
-    public int stock;
-     
-}
+    class VendingMachine
+    {
+        private List<Product> products = new List<Product>();
+        private int insertedMoney = 0;
+        private int totalMoney = 0;
+
+
+        public VendingMachine()
+        {
+            products.Add(new Product("ファンタ",10,160));
+            products.Add(new Product("グレープ", 10, 170));
+            products.Add(new Product("コーラ", 10, 180));
+        }
+        public void InsertMoney(int money)
+        {
+            insertedMoney += money;
+            Console.WriteLine($"{money}円を投入しました。現在の投入額: {insertedMoney}円");
+
+        }
+
+    }
+
+
 class Program
 {
     static void Main()
     {
-        
-        VendingMachine VM = new VendingMachine(2);
+
+        VendingMachine VM = new VendingMachine();
+        VM.InsertMoney(500);
         VM.Buy();
     }
 }
